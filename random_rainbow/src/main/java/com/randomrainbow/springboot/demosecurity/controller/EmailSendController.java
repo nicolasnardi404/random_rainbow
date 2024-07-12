d64c3ffc6b06a3fdb1122ac77bd347df5ed1a426
esquizo ciborgue <esquizociborgue@esquizos-MBP.homenet.telecomitalia.it>
2024-07-12 14:50:27 +0200

    create email verification

---
+++ random_rainbow/src/main/java/com/randomrainbow/springboot/demosecurity/controller/EmailSendController.java
@@ -0,0 +1,26 @@
+package com.randomrainbow.springboot.demosecurity.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.randomrainbow.springboot.demosecurity.service.EmailService;
+
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSendController {
+    private EmailService emailService;
+
+    public EmailSendController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public String sendMail(@RequestParam(value = "file", required = false) MultipartFile[] file, String to, String[] cc, String subject, String body) {
+        return emailService.sendMail(file, to, cc, subject, body);
+    }
+
+}
---
+++ random_rainbow/src/main/java/com/randomrainbow/springboot/demosecurity/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.randomrainbow.springboot.demosecurity.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface EmailService  {
+    String sendMail(MultipartFile[] file, String to,String[] cc, String subject, String body);
+}
